59dd12cd18e46973c648643edcf8b090cbc43734
Eder Canada <canada.eder@gmail.com>
2021-10-12 15:03:19 -0700

    axios unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get(`https://api.unsplash.com/search/photos`, {
+      params:{query: term},
+      headers:{
+        Authorization: 'Client-ID duFg4FuOQ59WE8ubtioncwLhYFFPr71WM--7gX-0nZU'
+      }
+    })
   }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
