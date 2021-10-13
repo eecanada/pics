0f1b1f5fe8905bb9a049c70851d139eb41827a61
Eder Canada <canada.eder@gmail.com>
2021-10-13 10:24:19 -0700

    call

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID duFg4FuOQ59WE8ubtioncwLhYFFPr71WM--7gX-0nZU',
+    Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`,
   },
 });
