695efa3cb38dfa2c170c1e4ba8e26edafafd0ba6
Eder Canada <canada.eder@gmail.com>
2021-10-13 09:55:04 -0700

    mvp

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-  const imgDiv = props.images.map(({ id, description, urls }) => {
-    return <img key={id} src={urls.regular} alt={description} />;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div className="image-list">{imgDiv}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
